53dd84876378d02fc5c35316aed53130b9e08333
Seth Foster <fosterbseth@gmail.com>
2021-02-03 02:43:30 -0500

    initial upload

---
+++ spec/which-class-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import WhichClassView from '../lib/which-class-view';
+
+describe('WhichClassView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
